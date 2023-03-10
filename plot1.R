bf9764a62609074df18a6a90d74202aefd04fbc1
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2023-03-10 20:10:50 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,20 +1,22 @@
 library("data.table")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+## Kaustubhya Shukla's work
 
-#Reads in data from file then subsets data for specified dates
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
+setwd("~/C:/Users/kaust/Downloads/datasciencecoursera-master/datasciencecoursera-master/4_Exploratory_Data_Analysis/week-1/peer-project1")
+
+
+powerDT1 <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?"
                              )
 
-# Prevents histogram from printing in scientific notation
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-# Change Date Column to Date Type
-powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+powerDT1[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+
+powerDT1[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
 # Filter Dates for 2007-02-01 and 2007-02-02
-powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+powerDT1 <- powerDT1[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 png("plot1.png", width=480, height=480)
 
